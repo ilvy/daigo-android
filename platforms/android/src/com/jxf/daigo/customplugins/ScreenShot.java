1e30fad97197ec908e36c19a53610b6d6ad1ad6a
ilvy <478283225@qq.com>
2016-09-07 22:33:12 +0800

    create

---
+++ platforms/android/src/com/jxf/daigo/customplugins/ScreenShot.java
@@ -0,0 +1,30 @@
+package com.jxf.daigo.customplugins;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * Created by gogo on 2016/8/29.
+ */
+public class ScreenShot extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("echo")) {
+            String message = args.getString(0);
+            this.echo(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void echo(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
